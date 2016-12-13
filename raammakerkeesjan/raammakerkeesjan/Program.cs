b060b15ea09517d1275a718cc63f4add7b7b26c4
Robby <robbyutregfan@hotmail.com>
2016-12-13 16:11:04 +0100

    kaashaha

--- raammakerkeesjan/raammakerkeesjan/Program.cs
+++ raammakerkeesjan/raammakerkeesjan/Program.cs
@@ -10,6 +10,27 @@ namespace raammakerkeesjan
     {
         static void Main(string[] args)
         {
+            int m_Square;
+            int m_Type;
+            int m_wood;
+            string m_keuzen;
+            Console.WriteLine("welkom, wat voor een soort raam zoekt u?");
+            Console.WriteLine("1.Standaart: 30 Euro");
+            Console.WriteLine("2.HR: 45 Euro");
+            Console.WriteLine("3.HR+: 50 Euro");
+            Console.WriteLine("4.HR++: 55 Euro");
+            m_Type = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("hoeveel vierkante meter wilt u?");
+            m_Square = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            Console.WriteLine("wilt u hout eromheen?");
+            
+            
+
+
+
+
         }
     }
 }
